fb32868e00a303b1c0042dac505049af0877d054
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-01-22 23:11:21 +0530

    Create Set1-1.c

---
+++ Set1-1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main(I)
+{
+int n;
+clrscr();
+scanf("%d",&n);
+if(n>0){
+printf("positive");
+}
+else if(n<0)
+{
+printf("negative");
+}
+else{
+printf("zero");
+}getch();
+}
