e369d5f5c1d46d955cda5041f65a13d11d6428c7
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-01-26 12:48:33 +0530

    Create beg3-28.c

---
+++ beg3-28.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,a[i];
+    clrscr();
+    printf("enter the array size");
+    scanf("%d",&n);
+    printf("enter the array value");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        }
+for(i=0;i<n;i++)
+{
+printf("%d %d\n",i,a[i]);
+}
+    getch();
+}
