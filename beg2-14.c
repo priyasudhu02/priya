2264212fa13272a0ae09e6120396bdc69052417f
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-01-26 12:14:22 +0530

    Create beg2-14.c

---
+++ beg2-14.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,i;
+ clrscr();
+ printf("enter a");
+ scanf("%d",&a);
+ printf("enter b");
+ scanf("%d",&b);
+ for(i=a+1;i<b;i++)
+ {
+     if(i%2!=0)
+     {
+         printf("%d\n",i);
+     }
+ }
+ 
+  getch();
+
+}
