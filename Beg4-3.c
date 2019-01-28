8c98c57c9a1623edc259984e15e195159b233983
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-01-28 22:20:46 +0530

    Create Beg4-3.c

---
+++ Beg4-3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+	intmain()
+	{
+	char a[100];
+	inti,count=0;
+	gets(a);
+	for(i=0;a[i]!='\0';i++)
+	{
+	if(a[i]=='')
+	{
+	count++;
+	}
+	}
+	printf("%d",count);
+	return0;
+	}
+
+
+
+
