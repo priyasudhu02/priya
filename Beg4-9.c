085c83820ea0d6cc79b98b4873aa15134812a9bd
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-01-28 22:07:00 +0530

    Create Beg4-9.c

---
+++ Beg4-9.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+	#include<conio.h>
+	voidmain()
+	{
+	int a[10],i,c,x;
+	clrscr();
+	printf("\narray elements");
+	for(i=0;i<10;i++)
+	{
+	scanf("%d",&a[i]);
+	}
+	c=a[0];
+	for(i=0;i<10;i++)
+	{
+	if(a[i]>c)
+	{
+	c=a[i];
+	}
+	}
+	printf("\n%d is highest",c);
+	getch();
+	}
+
+
