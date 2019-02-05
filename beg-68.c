700dcd9e522eb449aaa7b742bb16e80d6d2d5a87
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-05 20:54:32 +0530

    Create beg-68.c

---
+++ beg-68.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int p;
+clrscr();
+printf("enter the numbers");
+scanf("%d",&p);
+if(p%7==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
