a97ea50ea6aa89182417cff8fa0b7068987c92a9
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-08 22:17:46 +0530

    Update beg-93.c

--- beg-93.c
+++ beg-93.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
 int degree,deg;
@@ -8,5 +7,4 @@ printf("enter the degree");
 scanf("%d",&degree);
 deg=(degree-32)*5/9;
 printf("celsius is %d",deg);
-getch();
 }
