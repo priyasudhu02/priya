38d593ddaca57487adca2a986f7484e2dcdcbd08
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-12 15:32:26 +0530

    Create beg-105.c

---
+++ beg-105.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    long int n,m;
+    clrscr();
+    printf("enter n:");
+    scanf("%ld",&n);
+    printf("enter m:");
+    scanf("%ld",&m);
+    printf("%ld%ld",n,m);
+    }
