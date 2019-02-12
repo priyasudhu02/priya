f80390250892747d20eb0b5f03c982d831a21d0f
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-12 16:11:04 +0530

    Create beg-107.c

---
+++ beg-107.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c,d;
+    clrscr();
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    d=a*b/c;
+    printf("%d",d);
+    }
