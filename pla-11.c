0d09f9290003c073919e01149600cab175dd2e3f
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-20 15:46:23 +0530

    Create pla-11.c

---
+++ pla-11.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char a[20];
+    printf("enter the string:");
+    scanf("%s",a);
+    if(a[0]=='s')
+    {
+        printf("holiday");
+    }
+    else
+    {
+        printf("not a holiday");
+    }
+}
