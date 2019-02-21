3cdfd2564c7c1f347360fcf4dd863334639bb1b4
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-21 22:34:24 +0530

    Update pla-12.c

--- pla-12.c
+++ pla-12.c
@@ -1,26 +1,30 @@
-#include <stdio.h>
-
-int main()
+#include<stdio.h>
+#include<conio.h>
+void main()
 {
-    int a[50],i,n,k,temp;
-    printf("enter n:");
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    printf("enter k:");
-    scanf("%d",&k);
-    for(i=0;i<=k;i++)
-    {
-        temp=a[i];
-        a[i]=a[i+1];
-        a[i+1]=temp;
-    }
-    for(i=0;i<n;i++)
-    {
-        printf("%d",a[i]);
-    }
-
-    return 0;
+int a[10],i,k,n,j,temp;
+clrscr();
+printf("enter the array size");
+scanf("%d",&n);
+printf("enter the array elements");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
 }
+printf("enter the kth value");
+scanf("%d",&k);
+for(i=0;i<k;i++)
+{
+for(j=n-1;j>=0;j--)
+{
+temp=a[n-1];
+a[n-1]=a[j];
+a[j]=temp;
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d ",a[i]);
+}getch();
+}
+
