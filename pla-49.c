a43390f24d2ff88fd849db00af4cfdee7f00e982
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-26 23:49:28 +0530

    Update pla-49.c

--- pla-49.c
+++ pla-49.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 int main()
-{
-    long int n;
+{    long int n;
     scanf("%ld",&n);
     if(1<=n<=100000)
-    {
-        printf("INT");
+    {       printf("INT");
     }
     return 0;
 }
