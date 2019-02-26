7b35713b1ea1df6ea7440d2ef8942eef5a550d5e
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-26 23:47:32 +0530

    Update pla-48.c

--- pla-48.c
+++ pla-48.c
@@ -12,9 +12,6 @@ int main()
             if(i%2!=0)
             {
                 printf("%d ",i);
-            }
-        }
-    }
-    }
-    return 0;
+            }        } }
+    }  return 0;
 }
