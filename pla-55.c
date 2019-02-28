afb08df73c343bc37c8b0fd971cf2fe5317d2bdf
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-28 06:32:57 +0530

    Update pla-55.c

--- pla-55.c
+++ pla-55.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-int main()
-{
+void main(){
     char a[100],b[100];
     int i,c=0,n=0,j=0;
     scanf("%s",a);
@@ -36,5 +35,4 @@ int main()
     {
         printf("no");
     }
-    return 0;
 }
