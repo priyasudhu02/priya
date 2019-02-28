ebc5f3bf9edcb35cccc7aee7546a073cb3942ea9
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-02-28 06:36:09 +0530

    Update pla-57.c

--- pla-57.c
+++ pla-57.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-int main()
+void  main()
 {
     char a[100];
     char k;
@@ -14,5 +14,5 @@ int main()
             }
     }
     printf("%d",n);
-        return 0;
+      
 }
