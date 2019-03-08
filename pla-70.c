7b2640e55d188cc5f2941a39146c42629a4f2ebb
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-03-08 19:35:37 +0530

    Create pla-70.c

---
+++ pla-70.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[100000];
+    char b[6]="answer";
+    int i,l;
+    scanf("%s",a);
+    l=strlen(a);
+    a[l]=' ';
+    l++;
+    for(i=0;b[i]!='\0';i++)
+    {   
+        a[l]=b[i];
+        l++;
+    }
+    puts(a);
+}
