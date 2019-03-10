2de2e3f684abc1ee06ebd2f3ceb5c87fcfedfb5d
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-03-10 14:23:32 +0530

    Update pla-67.c

--- pla-67.c
+++ pla-67.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-void main()
-{
+void main(){
     int n,f=1,i;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         f=f*i;
     }
-    printf("%d",f);
-}
+    printf("%d",f);}
