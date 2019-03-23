d62eb79300a9e7a07463eee0a510b5c1955426bb
priyasudhu02 <46902393+priyasudhu02@users.noreply.github.com>
2019-03-23 21:15:43 +0530

    Create pla-128.c

---
+++ pla-128.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+    int n,a[100000],i,j,p=1;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+
+    {
+
+        scanf("%d",&a[i]);
+
+    }
+
+    for(i=0;i<n;i++)
+
+    {
+
+        p=p*a[i];
+
+    }
+
+    if(p%2==0)
+
+    {
+
+        printf("even");
+
+    }
+
+    else
+
+    {
+
+        printf("odd");
+
+    }
+}
